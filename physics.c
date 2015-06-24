6597e939c148ec6e606f5c5759368a6964bf3c82
Matt Pandina <artcfox@gmail.com>
2015-06-24 00:02:01 -0400

    Rearrange the physics code so it steps X before Y

--- physics.c
+++ physics.c
@@ -92,6 +92,9 @@ struct PLAYER_INFO {
 
   uint16_t framesFalling;
 
+  // Debugging purposes
+  bool clamped;
+
   /* u8 w; */
   /* u8 h; */
 };
@@ -132,8 +135,6 @@ u8 isSolid[] = {0, 0, 1, 0, 1, 1};
 #define p2vt(p) ((p) / TILE_HEIGHT)
 #define p2ht(p) ((p) / TILE_WIDTH)
 
-bool clamped[PLAYERS] = {0};
-
 /* Calculate forces that apply to the player
    Apply the forces to move and accelerate the player
    Collision detection (and resolution) */
@@ -161,46 +162,12 @@ void update(u8 i)
     player[i].jumping = true;
   }
 
-  // Integrate the Y forces to calculate the new position (x,y) and the new velocity (dx,dy)
-  player[i].y += (player[i].dy / WORLD_FPS);
-  player[i].dy += (player[i].ddy / WORLD_FPS);
-  if (player[i].dy < -WORLD_MAXDY)
-    player[i].dy = -WORLD_MAXDY;
-  else if (player[i].dy > WORLD_MAXDY)
-    player[i].dy = WORLD_MAXDY;
-
-  // Collision Detection for Y
-  u8 tx = p2ht(player[i].x);
-  u8 ty = p2vt(player[i].y);
-  u8 nx = player[i].x % TILE_WIDTH;  // true if player overlaps right
-  u8 ny = player[i].y % TILE_HEIGHT; // true if player overlaps below
-  u8 cell      = isSolid[GetTile(tx,     ty)];
-  u8 cellright = isSolid[GetTile(tx + 1, ty)];
-  u8 celldown  = isSolid[GetTile(tx,     ty + 1)];
-  u8 celldiag  = isSolid[GetTile(tx + 1, ty + 1)];
-
-  clamped[i] = false;
-  if (player[i].dy > 0) {
-    if ((celldown && !cell) ||
-        (celldiag && !cellright && nx)) {
-      clamped[i] = true;
-      player[i].y = vt2p(ty);     // clamp the y position to avoid falling into platform below
-      player[i].dy = 0;           // stop downward velocity
-      player[i].falling = false;  // no longer falling
-      player[i].framesFalling = 0;
-      player[i].jumping = false;  // (or jumping)
-      ny = 0;                     // no longer overlaps the cells below
-    }
-  } else if (player[i].dy < 0) {
-    if ((cell      && !celldown) ||
-        (cellright && !celldiag && nx)) {
-      clamped[i] = true;
-      player[i].y = vt2p(ty + 1); // clamp the y position to avoid jumping into platform above
-      player[i].dy = 0;           // stop updard velocity
-      ny = 0;                     // player no longer overlaps the cells below
-    }
+  // Clamp horizontal velocity to zero if we detect that the players direction has changed
+  if ((wasLeft && (player[i].dx > 0)) || (wasRight && (player[i].dx < 0))) {
+    player[i].dx = 0; // clamp at zero to prevent friction from making us jiggle side to side
   }
 
+  player[i].clamped = false;
 
   // Integrate the X forces to calculate the new position (x,y) and the new velocity (dx,dy)
   player[i].x += (player[i].dx / WORLD_FPS);
@@ -211,31 +178,32 @@ void update(u8 i)
     player[i].dx = WORLD_MAXDX;
 
   // Collision Detection for X
-  tx = p2ht(player[i].x);
-  ty = p2vt(player[i].y);
-  nx = player[i].x % TILE_WIDTH;  // true if player overlaps right
-  cell      = isSolid[GetTile(tx,     ty)];
-  cellright = isSolid[GetTile(tx + 1, ty)];
-  celldown  = isSolid[GetTile(tx,     ty + 1)];
-  celldiag  = isSolid[GetTile(tx + 1, ty + 1)];
+  u8 tx = p2ht(player[i].x);
+  u8 ty = p2vt(player[i].y);
+  u8 nx = player[i].x % TILE_WIDTH;  // true if player overlaps right
+  u8 ny = player[i].y % TILE_HEIGHT; // true if player overlaps below
+  u8 cell      = isSolid[GetTile(tx,     ty)];
+  u8 cellright = isSolid[GetTile(tx + 1, ty)];
+  u8 celldown  = isSolid[GetTile(tx,     ty + 1)];
+  u8 celldiag  = isSolid[GetTile(tx + 1, ty + 1)];
 
+  // When player[i].dx = 0, we should clamp from both sides to prevent overlapping adjacent cells
   if (player[i].dx >= 0) {
     if ((cellright && !cell) ||
         (celldiag  && !celldown && ny)) {
-      clamped[i] = true;
+      player[i].clamped = true;
       player[i].x = ht2p(tx);     // clamp the x position to avoid moving into the platform we just hit
       /* player[i].dx = 0;           // stop horizontal velocity */
       nx = 0;                     // player no longer overlaps the adjacent cell
       tx = p2ht(player[i].x);
       celldown  = isSolid[GetTile(tx,     ty + 1)];
       celldiag  = isSolid[GetTile(tx + 1, ty + 1)];
-
     }
   }
   if (player[i].dx <= 0) {
     if ((cell     && !cellright) ||
         (celldown && !celldiag && ny)) {
-      clamped[i] = true;
+      player[i].clamped = true;
       player[i].x = ht2p(tx + 1); // clamp the x position to avoid moving into the platform we just hit
       /* player[i].dx = 0;           // stop horizontal velocity */
       nx = 0;                     // player no longer overlaps the adjacent cell
@@ -245,9 +213,43 @@ void update(u8 i)
     }
   }
 
-  // Clamp horizontal velocity to zero if we detect that the players direction has changed
-  if ((wasLeft && (player[i].dx > 0)) || (wasRight && (player[i].dx < 0))) {
-    player[i].dx = 0; // clamp at zero to prevent friction from making us jiggle side to side
+  // Integrate the Y forces to calculate the new position (x,y) and the new velocity (dx,dy)
+  player[i].y += (player[i].dy / WORLD_FPS);
+  player[i].dy += (player[i].ddy / WORLD_FPS);
+  if (player[i].dy < -WORLD_MAXDY)
+    player[i].dy = -WORLD_MAXDY;
+  else if (player[i].dy > WORLD_MAXDY)
+    player[i].dy = WORLD_MAXDY;
+
+  // Collision Detection for Y
+  tx = p2ht(player[i].x);
+  ty = p2vt(player[i].y);
+  nx = player[i].x % TILE_WIDTH;  // true if player overlaps right
+  ny = player[i].y % TILE_HEIGHT; // true if player overlaps below
+  cell      = isSolid[GetTile(tx,     ty)];
+  cellright = isSolid[GetTile(tx + 1, ty)];
+  celldown  = isSolid[GetTile(tx,     ty + 1)];
+  celldiag  = isSolid[GetTile(tx + 1, ty + 1)];
+
+  if (player[i].dy > 0) {
+    if ((celldown && !cell) ||
+        (celldiag && !cellright && nx)) {
+      player[i].clamped = true;
+      player[i].y = vt2p(ty);     // clamp the y position to avoid falling into platform below
+      player[i].dy = 0;           // stop downward velocity
+      player[i].falling = false;  // no longer falling
+      player[i].framesFalling = 0;
+      player[i].jumping = false;  // (or jumping)
+      ny = 0;                     // no longer overlaps the cells below
+    }
+  } else if (player[i].dy < 0) {
+    if ((cell      && !celldown) ||
+        (cellright && !celldiag && nx)) {
+      player[i].clamped = true;
+      player[i].y = vt2p(ty + 1); // clamp the y position to avoid jumping into platform above
+      player[i].dy = 0;           // stop updard velocity
+      ny = 0;                     // player no longer overlaps the cells below
+    }
   }
 
   player[i].falling = !(celldown || (nx && celldiag)) && !player[i].jumping; // detect if we're now falling or not
@@ -336,12 +338,12 @@ int main()
     // Render the world
     for (u8 i = 0; i < PLAYERS; ++i) {
       if (player[i].left == player[i].right) {
-        if (clamped[i])
+        if (player[i].clamped)
           MapSprite2(i, orange_front, 0);
         else
           MapSprite2(i, yellow_front, 0);
       } else {
-        if (clamped[i])
+        if (player[i].clamped)
           MapSprite2(i, orange_side, player[i].right ? SPRITE_FLIP_X : 0);
         else
           MapSprite2(i, yellow_side, player[i].right ? SPRITE_FLIP_X : 0);
